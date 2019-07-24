3ede4d90475d98b9122a594a23b0d980186698fb
shanmingda <shanmingda@yunshuxie.com>
2019-07-24 23:57:42 +0800

    product商品服务

---
+++ src/main/java/com/smd/product/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.smd.product.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+
+    /**
+     * @Description: 商品list
+     *
+     * @param null
+     * @Return:
+     * @Author: shanmingda
+     * @Date: 2019-07-24 23:32
+    */
+
+    public void list() {
+
+    }
+}
